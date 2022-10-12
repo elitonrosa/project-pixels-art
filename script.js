4af6928d48ace9d8d503e082fef4b8d04e47e5d6
Eliton Da Rosa <elitondr@live.com>
2022-10-11 21:00:21 -0300

    Corrigido alguns erros Lint

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-window.onload = () => {
-  recoverColors();
-  // makeGrid();
-  recoverDraw();
-};
-
 const colors = document.querySelectorAll('#color-palette .color');
 let savedColors = [];
-let storage = localStorage;
+const storage = localStorage;
 
 function storageColors() {
   storage.setItem('colorPalette', JSON.stringify(savedColors));
@@ -14,15 +8,21 @@ function storageColors() {
 
 function recoverColors() {
   savedColors = JSON.parse(storage.getItem('colorPalette'));
-
   if (savedColors === null) {
     savedColors = ['black', 'red', 'green', 'blue'];
   }
-
   for (let index = 1; index < colors.length; index += 1) {
-    let color = savedColors[index];
+    const color = savedColors[index];
     colors[index].style.backgroundColor = `${color}`;
   }
+}
+function generateColor() {
+  const values = '0123456789ABCDEF';
+  let color = '#';
+  for (let index = 0; index < 6; index += 1) {
+    color += values[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 function setRandomColors() {
@@ -31,147 +31,116 @@ function setRandomColors() {
     colors[index].style.backgroundColor = `${color}`;
     savedColors[index] = color;
   }
-
   storageColors();
 }
 
-function generateColor() {
-  const values = '0123456789ABCDEF';
-  let color = '#';
-
-  for (let index = 0; index < 6; index += 1) {
-    color += values[Math.floor(Math.random() * 16)];
-  }
-
-  return color;
+function saveDraw() {
+  const draw = [];
+  const pixel = document.querySelectorAll('.pixel');
+  pixel.forEach((e) => {
+    const style = window.getComputedStyle(e);
+    draw.push(style.backgroundColor);
+  });
+  storage.setItem('pixelBoard', JSON.stringify(draw));
 }
 
 function paintPixel(color) {
-  let pixel = document.querySelectorAll('.pixel');
-
-  pixel.forEach(el => {
-    el.addEventListener('click', e => {
-      el.style.backgroundColor = color;
+  const pixel = document.querySelectorAll('.pixel');
+  pixel.forEach((el) => {
+    el.addEventListener('click', () => {
+      el.style.backgroundColor = color; 
       saveDraw();
-    })
-  })
+    });
+  });
 }
 
 function removeSelectedColor() {
-  colors.forEach(e => {
+  colors.forEach((e) => {
     e.classList.remove('selected');
   });
 }
 
 function selectNewColor() {
   let color;
-
-  colors.forEach(el => {
-    el.addEventListener('click', e => {
+  colors.forEach((el) => {
+    el.addEventListener('click', () => {
       removeSelectedColor();
       el.classList.add('selected');
       const styles = window.getComputedStyle(el);
       color = styles.backgroundColor;
       paintPixel(color);
-    })
-  })
+    });
+  });
 }
 
 function resetPixels() {
-  let pixel = document.querySelectorAll('.pixel');
-
-  pixel.forEach(e => {
+  const pixel = document.querySelectorAll('.pixel');
+  pixel.forEach((e) => {
     e.style.backgroundColor = 'white';
     saveDraw();
-  })
+  });
 }
 
-let draw = [];
-
-function saveDraw() {
-  draw = [];
-  let pixel = document.querySelectorAll('.pixel');
-
-
-  pixel.forEach(e => {
-    const style = window.getComputedStyle(e);
-    draw.push(style.backgroundColor);
-  })
-
-  storage.setItem('pixelBoard', JSON.stringify(draw));
+function makeGrid(grid = 5) {
+  const newGrid = (grid) * 45;
+  container.style.width = `${newGrid}px`;
+  const qtdPixels = grid * grid;
+  deleteGrid();
+  for (let i = 0; i < qtdPixels; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('pixel');
+    container.appendChild(newDiv);
+  }
+  // const pixel = document.querySelectorAll('.pixel');
+  paintPixel('black');
+  selectNewColor();
 }
 
 function recoverDraw() {
-  grid = parseInt(storage.getItem('boardSize'));
+  const grid = Number(storage.getItem('boardSize'));
   if (storage.getItem('boardSize') === null) {
     makeGrid(5);
   } else {
     makeGrid(grid);
   }
-
-
-  draw = JSON.parse(storage.getItem('pixelBoard'));
-  let pixel = document.querySelectorAll('.pixel');
-
-
+  const draw = JSON.parse(storage.getItem('pixelBoard'));
+  const pixel = document.querySelectorAll('.pixel');
   if (draw === null) {
-    return
+    return;
   }
-
   for (let i = 0; i < pixel.length; i += 1) {
     pixel[i].style.backgroundColor = draw[i];
   }
 }
 
 const input = document.querySelector('#board-size');
-let container = document.querySelector('#pixel-board');
+const container = document.querySelector('#pixel-board');
 
 function inputGrid() {
   if (input.value === '') {
     window.alert('Board inválido!');
     return;
   }
-
-  let grid = parseInt(input.value);
-
+  let grid = Number(input.value);
   if (grid < 5) {
     grid = 5;
   }
-
   if (grid > 50) {
     grid = 50;
   }
-
   storage.setItem('boardSize', `${grid}`);
   resetPixels();
-
   return makeGrid(grid);
 }
 
-function makeGrid(grid = 5) {
-  const newGrid = (grid) * 45;
-  container.style.width = `${newGrid}px`;
-  const qtdPixels = grid * grid
-
-  deleteGrid();
-
-  for (let i = 0; i < qtdPixels; i += 1) {
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('pixel');
-    container.appendChild(newDiv);
-  }
-
-
-  let pixel = document.querySelectorAll('.pixel');
-
-  paintPixel('black');
-  selectNewColor();
-}
-
 function deleteGrid() {
-  let pixel = document.querySelectorAll('.pixel');
-
+  const pixel = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixel.length; i += 1) {
     pixel[i].remove();
   }
 }
+
+window.onload = () => {
+  recoverColors();
+  recoverDraw();
+};
